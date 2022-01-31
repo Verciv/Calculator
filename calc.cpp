000177e6e4412c287e47e31b60daf519abe8941a
Vic <78374482+Verciv@users.noreply.github.com>
2022-01-31 11:41:03 -0800

    Update calc.cpp
    rewrite

--- calc.cpp
+++ calc.cpp
@@ -2,56 +2,78 @@
 #include <math.h>
 using namespace std;
 int main() {
-//Calculator
+    //Calculator
 
-//Variables
-double num1, num2;
-int choice;
+    //Variables
+    double num1, num2;
+    int choice;
+    bool x = true;
+    string s;
 
-//Input
-cout << "Enter number 1: ";
-cin >> num1;
-cout << "Enter number 2: ";
-cin >> num2;
+    while (x) {
+        //Input
+        cout << "Enter number 1: ";
+        cin >> num1;
+        cout << "Enter number 2: ";
+        cin >> num2;
 
-//Select Function
-cout << "Enter Your choice:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\nEnter Your choice number- ";
-cout << "Enter Your choice:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Modulus \nEnter Your choice number- ";
-cin>> choice;
-//Output
-switch(choice)
-{
+        //Select Function
+        cout << "Enter Your choice:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\nEnter Your choice number- ";
 
-//Addition
-case 1:
-cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
-break;
+        cin >> choice;
+        //Output
 
-//Subtraction
-case 2:
-cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
-break;
+        switch (choice)
+        {
 
-//Multiplication
-case 3:
-cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
-break;
+            //Addition
+        case 1:
+            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            break;
 
-//Division
-case 4:
-cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
-break;
+            //Subtraction
+        case 2:
+            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            break;
 
-//Error
+            //Multiplication
+        case 3:
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            break;
 
-case 5:
-cout<< num1 << " % " << num2 << " = " <<fmod(num1,num2) << endl;
-break;
+            //Division
+        case 4:
+            cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+            break;
 
-default:
-cout << "Invalid Entry!!";
-cout<<"want to enter more(y/n)";
-cin>>ch;
-}
-return 0;
+            //Error
+
+        case 5:
+            cout << num1 << " % " << num2 << " = " << fmod(num1, num2) << endl;
+            break;
+
+        default:
+            cout << "Invalid Entry!!";
+        }
+
+        cout << "Would you like to keep doing calculations? (y/n)" << endl;
+        cin >> s;
+        if (s == "y") {
+            cout << "Okay!" << endl;
+            x = true;
+        }
+        else if (s == "n") {
+            cout << "Goodbye!" << endl;
+            x = false;
+        }
+        else {
+            cout << "Goodbye!" << endl;
+            x = false;
+        }
+
+
+    }
+
+
+
 }
